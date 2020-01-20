067d78f881aaadfbfc4ee4eb9ea1e8d51e64bda9
gilangnazar <gilangnazar55@gmail.com>
2020-01-20 08:49:40 +0700

    modified gajiController.js

--- server/controller/gajiController.js
+++ server/controller/gajiController.js
@@ -5,32 +5,6 @@ const karyawan = require("../models/index").karyawan;
 gaji.belongsTo(karyawan, { foreignKey: "idKaryawan" });
 karyawan.belongsTo(jabatan, { foreignKey: "idJabatan" });
 
-// exports.getGaji = async (req, res) => {
-// 	try {
-// 		const data = await gaji.findAll({
-// 			include: [
-// 				{
-// 					model: karyawan,
-// 					include: [
-// 						{
-// 							model: gaji
-// 						}
-// 					]
-// 				}
-// 			]
-// 		});
-// 		if (data) {
-// 			res.status(200).json(data);
-// 		}
-// 	} catch (err) {
-// 		res.status(500).json({
-// 			status: "error",
-// 			messages: err.message,
-// 			data: {}
-// 		});
-// 	}
-// };
-
 exports.getGaji = async (req, res) => {
 	try {
 		const data = await gaji.findAll({
